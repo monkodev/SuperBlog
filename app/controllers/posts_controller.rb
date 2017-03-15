1e91c609e7638619cb0d539fd7264789b8f1e460
MonKoDev Technologies <gmalugani@gmail.com>
2017-03-15 15:55:57 -0300

    Modifica index - paneles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,17 @@ class PostsController < ApplicationController
 	def edit
 		@post = Post.find(params[:id])
 	end
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
+
+	protected
+	def post_params
+      params.require(:post).permit(:titulo, :texto)
+    end
 end
